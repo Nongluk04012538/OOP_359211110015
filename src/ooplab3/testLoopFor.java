21e47aab2fd43c3c18b55fb024beb7bdef30c678
nongluk04012538 <nongluk091@gmail.com>
2017-11-29 11:16:22 +0700

    loopfor

---
+++ src/ooplab3/testLoopFor.java
@@ -0,0 +1,14 @@
+package ooplab3;
+
+public class testLoopFor {
+    public static void main(String[] args) {
+        // Loop for
+        for(int i = 1;i<=100;i++){
+            System.out.println(i);
+
+
+
+
+        }//for
+    }
+}
