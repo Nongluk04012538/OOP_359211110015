38a0262c0a5c7cd9cc9811bc65e2b3f11f20c82f
Nongluk04012538 <nongluk091@gmail.com>
2017-12-13 11:08:01 +0700

    List

--- src/ooplab5/testArrayList.java
+++ src/ooplab5/testArrayList.java
@@ -1,8 +1,26 @@
 package ooplab5;
 
+import java.util.ArrayList;
+
 public class testArrayList {
     public static void main (String[] args) {
+//Arraylist
+        ArrayList list =new ArrayList();
+        list.add("SAM");
+        list.add("BOY");
+        list.add("GIRL");
+        System.out.println(list);
+        System.out.println(list.size());
+        list.remove(1);
+        System.out.println(list);
+        list.add(1,"joy");
+        System.out.println(list);
 
+
+        //print with for loop
+        for (int i=0;i<list.size();i++){
+            System.out.println(list.get(i));
+        }//for
 
 
 
