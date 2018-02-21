25c35c52fd468c4d009a4570664512a5ef6bec06
Nongluk04012538 <nongluk091@gmail.com>
2018-02-21 09:26:45 +0700

    TstSwtich

--- src/ooplab3/testSwtich.java
+++ src/ooplab3/testSwtich.java
@@ -2,19 +2,23 @@ package ooplab3;
 
 import java.util.Scanner;
 
-public class testSwtich {
-    public static void main(String[] args){
+public class TstSwtich {
+
+
+    public static void main (String[] args){
+
         Scanner scanner = new Scanner(System.in);
-        //switch-cass
+        //switch-case
+
         int select;
-        System.out.print("Please enter number 1-3");
+        System.out.print("Please enter number: ");
         select = scanner.nextInt();
         switch (select){
-            case 1: System.out.println("Your entered 1");break;
-            case 2: System.out.println("Your entered 2");break;
-            case 3: System.out.println("Your entered 3");break;
-            default: System.out.println("number 1-3 only.");
+            case 1: System.out.print("your entered 1");break;
+            case 2: System.out.print("your entered 2");break;
+            case 3: System.out.print("your entered 3");break;
+            default: System.out.print("Number 1-3 only.");
 
-        }
-    }
+        }//switch
+    }// main
 }
